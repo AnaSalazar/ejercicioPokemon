b5fb5bd52ab0c6ab461fb05612623b6aa1625bfc
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-03-28 23:10:50 -0600

    agrgando funciones en js y estructura HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,39 @@ function Pokemon(nombre,color,puntosAtaque){
   };
 }
 
+function crearPokemon(){
+  var nombrePokemon = document.getElementById("nombrePokemon");
+  var colorPokemon =  document.getElementById("colorPokemon");
+  var puntosAtaque =  document.getElementById("puntosAtaque");
 
-var pikachu = new Pokemon("Pikachu","amarillo",80);
+  var pokemon = new Pokemon(nombrePokemon.value,
+                            colorPokemon.value,
+                            Number(puntosAtaque.value)
+                           )
+  mostrarPokemon(pokemon);
+}
+
+function mostrarPokemon(pokemon) {
+    var opcionPokemon = document.getElementById("opcionPokemon");
+    var select1 = document.getElementById("pokemon1");
+    var select2 = document.getElementById("pokemon2");
+    var opcion1 = document.createElement("option");
+    var opcion2 = document.createElement("option");
+    opcion1.innerText = pokemon.nombre;
+    opcion2.innerText = pokemon.nombre;
+    select1.appendChild(opcion1);
+    select2.appendChild(opcion2);
+}
+
+
+function aPelear(pokemon) {
+  var pokemon1 = document.getElementById("pokemon1").value;
+  var pokemon2 = document.getElementById("pokemon2").value;
+  pokemon1.pelear(pokemon2);
+}
+/*var pikachu = new Pokemon("Pikachu","amarillo",80);
 var charmander  = new Pokemon("Charmander","rojo",30);
 
 pikachu.pelear(charmander)
 console.log(charmander.vida)
+*/
